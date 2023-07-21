dc76ec50355717a1d05760770b89241d04009b65
William Addesso <wsaddesso@gmail.com>
2023-07-21 15:24:50 -0400

    Audio now plays on loop when Play is clicked. Pauses when Pause is clicked.

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,16 +1,37 @@
 import beatAudio from '../audio/BPM.mp3';
+import { useRef, useState } from 'react';
+import '../styling/audio.css';
 
 const Audio = () => {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  // When Play is clicked, audio plays on loop
+  // TODO: It needs to play once per beat per minute.
   const handlePlay = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log('Playing');
+    if (isPlaying) {
+      audioRef.current.pause();
+      console.log('Paused');
+    } else {
+      audioRef.current.play();
+      console.log('Playing');
+    }
+    setIsPlaying(!isPlaying);
   };
 
   return (
     <div>
-      <audio controls play="true" src={beatAudio} type="audio/mpeg"></audio>
-      <button onClick={handlePlay}>Play</button>
+      <audio
+        ref={audioRef}
+        controls
+        play="true"
+        src={beatAudio}
+        type="audio/mpeg"
+        loop
+      ></audio>
+      <button onClick={handlePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
     </div>
   );
 };
